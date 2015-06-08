a302b40609a139707df8c213faca54dfa1022549
Joe Barnes <barnesjd@gmail.com>
2015-06-07 21:28:46 -0500

    Adding Bool2Specs

---
+++ src/test/scala/com.joescii.typeprog/Bool2Specs.scala
@@ -0,0 +1,15 @@
+package com.joescii.typeprog
+
+import shapeless.test.illTyped
+
+object Bool2Specs {
+  implicitly[TrueType#And[TrueType] =:= TrueType]
+  implicitly[TrueType#And[FalseType] =:= FalseType]
+  implicitly[FalseType#And[TrueType] =:= FalseType]
+  implicitly[FalseType#And[FalseType] =:= FalseType]
+
+  illTyped("implicitly[TrueType#And[TrueType] =:= FalseType]")
+  illTyped("implicitly[TrueType#And[FalseType] =:= TrueType]")
+  illTyped("implicitly[FalseType#And[TrueType] =:= TrueType]")
+  illTyped("implicitly[FalseType#And[FalseType] =:= TrueType]")
+}
