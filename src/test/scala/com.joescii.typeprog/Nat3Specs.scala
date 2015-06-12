da17b783a2c0f16d093f4705f0ea0e523b407440
Joe Barnes <barnesjd@gmail.com>
2015-06-12 16:33:30 -0500

    Made the challenge more specific => Generate a StackOverflowError

--- src/test/scala/com.joescii.typeprog/Nat3Specs.scala
+++ src/test/scala/com.joescii.typeprog/Nat3Specs.scala
@@ -1,5 +1,5 @@
 package com.joescii.typeprog
 
 object Nat3Specs {
-  // Can you generate a Nat so large that it breaks the compiler?
+  // Can you generate a Nat so large that it breaks the compiler with a java.lang.StackOverflowError?
 }
