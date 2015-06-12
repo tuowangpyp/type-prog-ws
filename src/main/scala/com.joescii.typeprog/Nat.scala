ecf9c9dc3e111cad24bbfc80a80859c7e224203e
Joe Barnes <barnesjd@gmail.com>
2015-06-12 16:09:19 -0500

    Solution for nat-2

--- src/main/scala/com.joescii.typeprog/Nat.scala
+++ src/main/scala/com.joescii.typeprog/Nat.scala
@@ -2,10 +2,13 @@ package com.joescii.typeprog
 
 sealed trait Nat {
   type plus[That <: Nat] <: Nat
+  type times[That <: Nat] <: Nat
 }
 sealed trait Nat0 extends Nat {
   type plus[That <: Nat] = That
+  type times[That <: Nat] = Nat0
 }
 sealed trait NatN[Prev <: Nat] extends Nat {
   type plus[That <: Nat] = NatN[Prev#plus[That]]
+  type times[That <: Nat] = That#plus[That#times[Prev]]
 }
