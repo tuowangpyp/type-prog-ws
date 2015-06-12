c5f8f2e9160c573c10ef0b1f51ae8a439aebe48d
Joe Barnes <barnesjd@gmail.com>
2015-06-12 15:59:45 -0500

    Solution to bool-1

--- src/main/scala/com.joescii.typeprog/Bool.scala
+++ src/main/scala/com.joescii.typeprog/Bool.scala
@@ -1,6 +1,15 @@
 package com.joescii.typeprog
 
-sealed trait Bool
-sealed trait True extends Bool
-sealed trait False extends Bool
+sealed trait Bool {
+  type Not <: Bool
+  type Or[That <: Bool] <: Bool
+}
+sealed trait True extends Bool {
+  type Not = False
+  type Or[That <: Bool] = True
+}
+sealed trait False extends Bool {
+  type Not = True
+  type Or[That <: Bool] = That
+}
 
