e5a0882f6f42641162b7016484ca9c63db17d4fd
Joe Barnes <barnesjd@gmail.com>
2015-06-13 14:29:01 -0500

    Solution for vect-1

--- src/main/scala/com.joescii.typeprog/Vector.scala
+++ src/main/scala/com.joescii.typeprog/Vector.scala
@@ -1,5 +1,25 @@
 package com.joescii.typeprog
 
-sealed trait Vector
-case object VNil
-case class NonEmptyVector()
+sealed trait Vector {
+  def size:Int
+  def ::(head:Int):Vector = NonEmptyVector(head, this)
+  def +(that:Vector):Vector
+}
+
+case object VNil extends Vector  {
+  val size = 0
+  def +(that:Vector) = {
+    require(that == this)
+    VNil
+  }
+}
+
+case class NonEmptyVector(head:Int, tail:Vector) extends Vector {
+  val size = 1 + tail.size
+  def +(that:Vector) = {
+    require(that.size == this.size)
+    that match {
+      case NonEmptyVector(head2, tail2) => NonEmptyVector(head + head2, tail + tail2)
+    }
+  }
+}
