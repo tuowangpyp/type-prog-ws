e613be0e417f79ec379d023a80b925a55cb49d8a
Joe Barnes <barnesjd@gmail.com>
2015-06-14 10:09:04 -0500

    Adding the FlatSpec stuff to Vect1Spec

--- src/test/scala/com.joescii.typeprog/Vect1Specs.scala
+++ src/test/scala/com.joescii.typeprog/Vect1Specs.scala
@@ -3,10 +3,14 @@ package com.joescii.typeprog
 import org.scalatest.{ FlatSpec, Matchers }
 
 class Vect1Specs extends FlatSpec with Matchers {
-  val sum = (1 :: 2 :: VNil) + (3 :: 4 :: VNil)
-  sum should equal (4 :: 6 :: VNil)
+  "Vector addition" should "compute [1, 2] + [3, 4] => [4, 6]" in {
+    val sum = (1 :: 2 :: VNil) + (3 :: 4 :: VNil)
+    sum should equal(4 :: 6 :: VNil)
+  }
 
-  intercept[IllegalArgumentException](
-    (1 :: 2 :: VNil) + (5 :: VNil)
-  )
+  "Vector addition" should "reject [1, 2] + [5] with an IllegalArgumentException" in {
+    intercept[IllegalArgumentException](
+      (1 :: 2 :: VNil) + (5 :: VNil)
+    )
+  }
 }
