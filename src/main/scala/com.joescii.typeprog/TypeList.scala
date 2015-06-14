93caa2a17e39dafead3ead64a534676030d8b07b
Joe Barnes <barnesjd@gmail.com>
2015-06-14 13:52:40 -0500

    Implemented the reduce type

--- src/main/scala/com.joescii.typeprog/TypeList.scala
+++ src/main/scala/com.joescii.typeprog/TypeList.scala
@@ -2,10 +2,13 @@ package com.joescii.typeprog
 
 sealed trait TypeList {
   type size <: Nat
+  type reduce <: Nat
 }
 sealed trait TNil extends TypeList {
   type size = Nat0
+  type reduce = Nat0
 }
-sealed trait ::[H, T <: TypeList] extends TypeList {
+sealed trait ::[H <: Nat, T <: TypeList] extends TypeList {
   type size = NatN[T#size]
+  type reduce = H#plus[T#reduce]
 }
