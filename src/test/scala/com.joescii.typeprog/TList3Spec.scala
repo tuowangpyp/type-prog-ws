d96c01348ad22a7cdfaeb1f60836a102ac9dd249
Joe Barnes <barnesjd@gmail.com>
2015-06-14 14:26:17 -0500

    Adding a second usage for good measure

--- src/test/scala/com.joescii.typeprog/TList3Spec.scala
+++ src/test/scala/com.joescii.typeprog/TList3Spec.scala
@@ -4,9 +4,12 @@ object TList3Spec {
   type Nat1 = NatN[Nat0]
   type Nat2 = NatN[Nat1]
   type Nat3 = NatN[Nat2]
+  type Nat4 = NatN[Nat3]
 
   type List1 = Nat0 :: Nat1 :: Nat2 :: TNil
   type List2 = Nat1 :: Nat2 :: Nat3 :: TNil
+  type List3 = Nat2 :: Nat3 :: Nat4 :: TNil
 
   implicitly[List1#map[({type F[i <: Nat] = NatN[i]})#F] =:= List2]
+  implicitly[List1#map[({type F[i <: Nat] = NatN[NatN[i]])#F] =:= List3]
 }
