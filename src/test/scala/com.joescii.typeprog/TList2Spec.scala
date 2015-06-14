bee692360f8e11dfe7b104c0c8959365eb7d5aa9
Joe Barnes <barnesjd@gmail.com>
2015-06-14 13:48:26 -0500

    Changing the exercise to reduce

--- src/test/scala/com.joescii.typeprog/TList2Spec.scala
+++ src/test/scala/com.joescii.typeprog/TList2Spec.scala
@@ -4,9 +4,7 @@ object TList2Spec {
   type Nat1 = NatN[Nat0]
   type Nat2 = NatN[Nat1]
   type Nat3 = NatN[Nat2]
+  type Nat6 = NatN[NatN[NatN[Nat3]]]
 
-  type List1 = Nat0 :: Nat0 :: Nat1 :: TNil
-  type List2 = Nat3 :: Nat1 :: Nat1 :: TNil
-
-  implicitly[List1#plus[List2] =:= (Nat3 :: Nat1 :: Nat2 :: TNil)]
+  implicitly[(Nat1 :: Nat2 :: Nat3 :: TypeList)#reduce =:= Nat6]
 }
