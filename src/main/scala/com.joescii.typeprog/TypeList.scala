73183f079d5b6ee1e9eff223af11c170082fac10
Joe Barnes <barnesjd@gmail.com>
2015-06-16 20:36:09 -0500

    Adding bounds to my type parameter for map's F

--- src/main/scala/com.joescii.typeprog/TypeList.scala
+++ src/main/scala/com.joescii.typeprog/TypeList.scala
@@ -3,15 +3,15 @@ package com.joescii.typeprog
 sealed trait TypeList {
   type size <: Nat
   type reduce <: Nat
-  type map[F[_] <: Nat] <: TypeList
+  type map[F[_ <: Nat] <: Nat] <: TypeList
 }
 sealed trait TNil extends TypeList {
   type size = Nat0
   type reduce = Nat0
-  type map[F[_] <: Nat] = TNil
+  type map[F[_ <: Nat] <: Nat] = TNil
 }
 sealed trait ::[H <: Nat, T <: TypeList] extends TypeList {
   type size = NatN[T#size]
   type reduce = H#plus[T#reduce]
-  type map[F[_] <: Nat] = F[H] :: T#map[F]
+  type map[F[_ <: Nat] <: Nat] = F[H] :: T#map[F]
 }
