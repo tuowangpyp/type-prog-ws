af01db221f41a048ab0b6ec15ad2ce1dafd82000
Joe Barnes <barnesjd@gmail.com>
2015-06-16 20:41:43 -0500

    Implemented TypeList#fold

--- src/main/scala/com.joescii.typeprog/TypeList.scala
+++ src/main/scala/com.joescii.typeprog/TypeList.scala
@@ -4,14 +4,17 @@ sealed trait TypeList {
   type size <: Nat
   type reduce <: Nat
   type map[F[_ <: Nat] <: Nat] <: TypeList
+  type fold[A <: Nat, F[_ <: Nat, _ <: Nat] <: Nat] <: Nat
 }
 sealed trait TNil extends TypeList {
   type size = Nat0
   type reduce = Nat0
   type map[F[_ <: Nat] <: Nat] = TNil
+  type fold[A <: Nat, F[_ <: Nat, _ <: Nat] <: Nat] = A
 }
 sealed trait ::[H <: Nat, T <: TypeList] extends TypeList {
   type size = NatN[T#size]
   type reduce = H#plus[T#reduce]
   type map[F[_ <: Nat] <: Nat] = F[H] :: T#map[F]
+  type fold[A <: Nat, F[_ <: Nat, _ <: Nat] <: Nat] = F[H, T#fold[A, F]]
 }
