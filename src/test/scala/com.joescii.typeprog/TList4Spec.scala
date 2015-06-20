10e29651bfd0ef0523f5d027b2eba10ceb59d1c2
Joe Barnes <barnesjd@gmail.com>
2015-06-20 11:09:15 -0500

    Using Nat operators

--- src/test/scala/com.joescii.typeprog/TList4Spec.scala
+++ src/test/scala/com.joescii.typeprog/TList4Spec.scala
@@ -1,5 +1,7 @@
 package com.joescii.typeprog
 
+import Nat._
+
 object TList4Spec {
   type Nat1 = NatN[Nat0]
   type Nat2 = NatN[Nat1]
@@ -8,8 +10,8 @@ object TList4Spec {
   type Nat6 = NatN[NatN[Nat4]]
 
   type List1 = Nat2 :: Nat1 :: Nat0 :: Nat1 :: TNil
-  implicitly[List1#fold[Nat0, ({type F[a <: Nat, b <: Nat] = a#plus[b]})#F] =:= Nat4]
+  implicitly[List1#fold[Nat0, ({type F[a <: Nat, b <: Nat] = a + b})#F] =:= Nat4]
 
   type List2 = Nat1 :: Nat1 :: Nat3 :: Nat2 :: Nat1 :: TNil
-  implicitly[List2#fold[Nat1, ({type F[a <: Nat, b <: Nat] = a#times[b]})#F] =:= Nat6]
+  implicitly[List2#fold[Nat1, ({type F[a <: Nat, b <: Nat] = a * b})#F] =:= Nat6]
 }
