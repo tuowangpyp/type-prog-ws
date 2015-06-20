5288c7b9be06ed28b795a1fed70a3cdda8942a2b
Joe Barnes <barnesjd@gmail.com>
2015-06-20 10:30:58 -0500

    Adding a + operator

--- src/test/scala/com.joescii.typeprog/Nat1Specs.scala
+++ src/test/scala/com.joescii.typeprog/Nat1Specs.scala
@@ -2,6 +2,8 @@ package com.joescii.typeprog
 
 import shapeless.test.illTyped
 
+import Nat._
+
 object Nat1Specs {
   type Nat1 = NatN[Nat0]
   type Nat2 = NatN[Nat1]
@@ -13,4 +15,8 @@ object Nat1Specs {
   implicitly[Nat0#plus[Nat1] =:= Nat1]
   implicitly[Nat1#plus[Nat1] =:= Nat2]
   implicitly[Nat1#plus[Nat2] =:= Nat3]
+
+  implicitly[Nat0 + Nat1 =:= Nat1]
+  implicitly[Nat1 + Nat1 =:= Nat2]
+  implicitly[Nat1 + Nat2 =:= Nat3]
 }
