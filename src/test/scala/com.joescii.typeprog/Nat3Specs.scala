5bb3a829d0cc02966d208feceac45d99a572d4aa
Tuo Wang <tuowangpylp@gmail.com>
2015-06-26 15:58:05 -0500

    Fixed the Nat-3

--- src/test/scala/com.joescii.typeprog/Nat3Specs.scala
+++ src/test/scala/com.joescii.typeprog/Nat3Specs.scala
@@ -2,4 +2,10 @@ package com.joescii.typeprog
 
 object Nat3Specs {
   // Can you generate a Nat so large that it breaks the compiler with a java.lang.StackOverflowError?
+//  type Nat1 = NatN[Nat0]
+//  type Nat2 = NatN[Nat1]
+//  type Nat3 = NatN[Nat2]
+//  type Nat6 = NatN[NatN[NatN[Nat3]]]
+//
+//  implicitly[Nat6#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6]#times[Nat6] =:= Nat6]
 }
