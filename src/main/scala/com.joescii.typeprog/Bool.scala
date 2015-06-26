1b7177348b7e2df1ee6f70d8d53378e6e5a4f973
Tuo Wang <tuowangpylp@gmail.com>
2015-06-26 13:33:58 -0500

    Fixed the Bool-3

--- src/main/scala/com.joescii.typeprog/Bool.scala
+++ src/main/scala/com.joescii.typeprog/Bool.scala
@@ -4,15 +4,18 @@ sealed trait Bool {
   type Not <: Bool
   type Or[That <: Bool] <: Bool
   type And[That <: Bool] <: Bool
+  type Imp[That <: Bool] <: Bool
 }
 sealed trait True extends Bool {
   type Not = False
   type Or[That <: Bool] = True
   type And[That <: Bool] = That
+  type Imp[That <: Bool] = That
 }
 sealed trait False extends Bool {
   type Not = True
   type Or[That <: Bool] = That
   type And[That <: Bool] = False
+  type Imp[That <: Bool] = True
 }
 
